9eedde0c30b77290114cbfd4d598251b88700384
BIRGO <abdoulebgaa8@gmail.com>
2023-05-11 19:03:16 +0100

    reading x, y and z values

--- exo12.c
+++ exo12.c
@@ -5,6 +5,15 @@
 int main ()
 {
     printf("hello world!");
+    int x, y ,z;
+    x = y = z = 0;
+    printf("enter x, y and z values\n ");
+    scanf("x = %d",x);
+    scanf("y = %d",y);
+    scanf("z = %d",z);
+    printf("x = %d y = %d z = %d\n",x,y,z); 
     
+
+
     return 0;
 }
